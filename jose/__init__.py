80c016ae519f49f783c20b85a6ec01ad49780024
Ben Mather <bwhmather@bwhmather.com>
2014-12-24 20:20:19 +0000

    move base64 stuff into utils module

--- jose/__init__.py
+++ jose/__init__.py
@@ -10,8 +10,6 @@ except ImportError:  # pragma: nocover
 import zlib
 import datetime
 
-from base64 import urlsafe_b64encode, urlsafe_b64decode
-import binascii
 from collections import namedtuple
 from time import time
 
@@ -19,13 +17,7 @@ from Crypto.Random import get_random_bytes
 
 from jose import algorithms
 from jose.exceptions import Error, Expired, NotYetValid
-
-
-try:
-    # python 2 compatibility
-    unicode
-except NameError:
-    unicode = str
+from jose.utils import b64encode_url, b64decode_url
 
 
 __all__ = ['encrypt', 'decrypt', 'sign', 'verify']
@@ -283,40 +275,6 @@ def verify(jws, jwk, validate_claims=True, expiry_seconds=None):
     _validate(claims, validate_claims, expiry_seconds)
 
     return JWT(header, claims)
-
-
-def b64decode_url(istr):
-    """ JWT Tokens may be truncated without the usual trailing padding '='
-        symbols. Compensate by padding to the nearest 4 bytes.
-
-    :param istr: A unicode string to decode
-    :returns: The byte string represented by `istr`
-    """
-    # unicode check for python 2 compatibility
-    if not isinstance(istr, (str, unicode)):
-        raise ValueError("expected string, got %r" % type(istr))
-
-    # required for python 2 as urlsafe_b64decode does not like unicode objects
-    # safe as b64 encoded string should be only ascii anyway
-    istr = str(istr)
-
-    try:
-        return urlsafe_b64decode(istr + '=' * (4 - (len(istr) % 4)))
-    except (TypeError, binascii.Error) as e:
-        raise Error('Unable to decode base64: %s' % (e))
-
-
-def b64encode_url(istr):
-    """ JWT Tokens may be truncated without the usual trailing padding '='
-        symbols. Compensate by padding to the nearest 4 bytes.
-
-    :param istr: a byte string to encode
-    :returns: The base64 representation of the input byte string as a regular
-        `str` object
-    """
-    if not isinstance(istr, bytes):
-        raise Exception("expected bytestring")
-    return urlsafe_b64encode(istr).rstrip(b'=').decode('ascii')
 
 
 COMPRESSION = {
--- jose/utils.py
+++ jose/utils.py
@@ -1,3 +1,16 @@
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+import binascii
+
+from jose.exceptions import Error
+
+
+try:
+    # python 2 compatibility
+    unicode
+except NameError:
+    unicode = str
+
+
 def pad_pkcs7(s, block_size):
     padding = block_size - (len(s) % block_size)
     # TODO would be cleaner to do `bytes(sz) * sz` but python 2 behaves
@@ -6,6 +19,7 @@ def pad_pkcs7(s, block_size):
 
 
 def unpad_pkcs7(s):
+    # TODO validation
     try:
         return s[:-ord(s[-1])]
     # Python 3 compatibility
@@ -27,3 +41,37 @@ def const_compare(stra, strb):
     for a, b in zip(orda, ordb):
         res |= a ^ b
     return res == 0
+
+
+def b64decode_url(istr):
+    """ JWT Tokens may be truncated without the usual trailing padding '='
+        symbols. Compensate by padding to the nearest 4 bytes.
+
+    :param istr: A unicode string to decode
+    :returns: The byte string represented by `istr`
+    """
+    # unicode check for python 2 compatibility
+    if not isinstance(istr, (str, unicode)):
+        raise ValueError("expected string, got %r" % type(istr))
+
+    # required for python 2 as urlsafe_b64decode does not like unicode objects
+    # safe as b64 encoded string should be only ascii anyway
+    istr = str(istr)
+
+    try:
+        return urlsafe_b64decode(istr + '=' * (4 - (len(istr) % 4)))
+    except (TypeError, binascii.Error) as e:
+        raise Error('Unable to decode base64: %s' % (e))
+
+
+def b64encode_url(istr):
+    """ JWT Tokens may be truncated without the usual trailing padding '='
+        symbols. Compensate by padding to the nearest 4 bytes.
+
+    :param istr: a byte string to encode
+    :returns: The base64 representation of the input byte string as a regular
+        `str` object
+    """
+    if not isinstance(istr, bytes):
+        raise Exception("expected bytestring")
+    return urlsafe_b64encode(istr).rstrip(b'=').decode('ascii')
