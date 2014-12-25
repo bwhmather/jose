066076e7d11e7ffce8b533054147bbe0617f1b5b
Ben Mather <bwhmather@bwhmather.com>
2014-12-25 00:48:05 +0000

    make and Expired and NotYetValid subclasses of TimingError

--- jose/exceptions.py
+++ jose/exceptions.py
@@ -4,13 +4,19 @@ class Error(Exception):
     pass
 
 
-class Expired(Error):
+class TimingError(Error):
+    """ Raised if JWT is not valid at the requested time
+    """
+    pass
+
+
+class Expired(TimingError):
     """ Raised during claims validation if a JWT has expired
     """
     pass
 
 
-class NotYetValid(Error):
+class NotYetValid(TimingError):
     """ Raised during claims validation is a JWT is not yet valid
     """
     pass
